251de362f8d8318b16dd59c92fa921bd688942f0
jewelEarthDeveloper <paul.spencer@student.uva.nl>
2020-02-14 15:08:31 +0100

    some small changes that were somehonw missing

--- python-flask-server-generated/python-flask-server/swagger_server/util.py
+++ python-flask-server-generated/python-flask-server/swagger_server/util.py
@@ -3,18 +3,15 @@ import datetime
 import six
 import typing
 
-
 def _deserialize(data, klass):
     """Deserializes dict, list, str into an object.
-
     :param data: dict, list or str.
     :param klass: class literal, or string of class name.
-
     :return: object.
     """
     if data is None:
         return None
-
+ 
     if klass in six.integer_types or klass in (float, str, bool):
         return _deserialize_primitive(data, klass)
     elif klass == object:
@@ -23,10 +20,10 @@ def _deserialize(data, klass):
         return deserialize_date(data)
     elif klass == datetime.datetime:
         return deserialize_datetime(data)
-    elif type(klass) == typing.GenericMeta:
-        if klass.__extra__ == list:
+    elif hasattr(klass, '__origin__'):
+        if klass.__origin__ == list:
             return _deserialize_list(data, klass.__args__[0])
-        if klass.__extra__ == dict:
+        if klass.__origin__ == dict:
             return _deserialize_dict(data, klass.__args__[1])
     else:
         return deserialize_model(data, klass)
